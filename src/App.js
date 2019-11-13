7dc014e8fcd3fc8dd51cab9c6cfb38954c247c10
jakebahr <bydesignwebsystems@gmail.com>
2019-11-13 13:56:06 -0500

    implement context api

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/layout/pages/About';
 import axios from 'axios';
+
+import GithubState from './context/github/GithubState'
+
 import './App.css';
 
 const App = () => {
@@ -68,6 +71,7 @@ const getUserRepos = async username => {
   };
 
     return (
+      <GithubState>
       <Router>
       <div className='App'>
         <Navbar />
@@ -88,6 +92,7 @@ const getUserRepos = async username => {
         </div>
       </div>
       </Router>
+      </GithubState>
     ); 
 }
 
