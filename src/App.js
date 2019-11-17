8713929adb672e5db9d30592c0398f285be584e1
Jacob Bahr <52383502+jakebahr@users.noreply.github.com>
2019-11-16 20:39:55 -0500

    test

--- src/App.js
+++ src/App.js
@@ -27,16 +27,6 @@ const App = () => {
   //   this.setState({ users: res.data, loading: false });
   // }
 
-  //Search Github Users
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-  setUsers(res.data.items);
-  setLoading(false);
-  };
-
 
   //Get User Repos
 const getUserRepos = async username => {
@@ -80,7 +70,7 @@ const getUserRepos = async username => {
           <Switch>
             <Route exact path='/' render={props => ( 
             <Fragment>
-              <Search searchUsers={searchUsers} clearUsers={clearUsers} showClear={users.length > 0 ? true: false} setAlert={showAlert} />
+              <Search clearUsers={clearUsers} showClear={users.length > 0 ? true: false} setAlert={showAlert} />
               <Users loading={loading} users={users} />
             </Fragment>
             )} />
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -9,7 +9,7 @@ import {
     GET_USER,
     GET_REPOS
 } from '../types';
-
+ 
 const GithubState = props => {
     const initialState = {
         users: [],
@@ -20,18 +20,30 @@ const GithubState = props => {
 
     const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-    //Search User
+    //Search Github User
+  const searchUsers = async text => {
+    setLoading();
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    dispatch({
+        type: SEARCH_USERS,
+        payload: res.data.items
+    });
+};
     //Get User
     //Get Repos
     //Clear Users
     //Set Loading
+    const setLoading = () => dispatch({ type: SET_LOADING });
 
     return <GithubContext.Provider
         value={{
             users: state.users,
             user: state.user,
             repos: state.repos,
-            loading: state.loading
+            loading: state.loading,
+            searchUsers
         }}
     >
         {props.children}
